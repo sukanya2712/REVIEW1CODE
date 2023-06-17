07953c569f38b4f86093016432a0b81d18d991c8
sukanya2712 <sukanyanaik511@gmail.com>
2023-06-17 21:25:19 +0530

    added

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace review1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] arr = { 1, 2, 3, 7, 8, 10, 9 };
+            int count = 0;
+            int sum = 0;
+            for (int i = arr.Length-1; i >0; i--)
+            {
+               
+                if (arr[i] %2 == 0 && count <2)
+                {
+                    sum += arr[i];
+                    count++;
+             
+
+                }
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
